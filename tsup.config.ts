9f9d48f43f84315601bc2d2ba95c0fcf30832fe3
Alex MacCaw <alex@alexmaccaw.com>
2023-08-17 15:57:14 -0400

    ensure zod is bundled

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'tsup'
 
 export default defineConfig({
-  entry: ['src/index.ts'],
+  entry: ['src/index.ts', 'src/middleware/zod-validation.ts'],
   format: ['esm', 'cjs'],
   clean: true,
   dts: true,
